b21ef956c3a98b5970119b4e132e345db4a7758a
Tommy Egbe <tabe_e@yahoo.ca>
2022-05-07 13:10:09 +0200

    added an array containing a list of pokemons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-alert('Hello world!');
-let favoriteFood= "Kebab Döner";
-document.write(favoriteFood);
+let pokemonList=[
+    {
+        name:'Bulbasaur',
+        height:0.7,
+        types:'grass'
+    },
+    {
+        name:'Ivysaur',
+        height:1,
+        types:'poison'
+    },
+    {
+        name:"Venusaur",
+        height:2,
+        types:'monster'
+    }
+];
+console.log(pokemonList)
