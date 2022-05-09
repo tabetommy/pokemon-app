a9010680d05d19f0c5f44f120b8f1359f6cbde02
Tommy Egbe <tabe_e@yahoo.ca>
2022-05-09 16:02:42 +0200

    added IIFE and edited forEach mehtod accordingly

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-let pokemonList=[
+let pokemonRepository=(
+    function(){
+
+        let pokemonList=[
     {
         name:'Bulbasaur',
         height:0.7,
@@ -19,9 +22,49 @@ let pokemonList=[
         height:2.5,
         types:'Lizard'
     }
-];
+    ];
 
-pokemonList.forEach(pokemon=>{
+    //comparing two arrays if they are equal(for validation purposes)
+    function areEqual(array1, array2) {
+      if (array1.length === array2.length) {
+        return array1.every((element, index) => {
+          if (element === array2[index]) {
+            return true;
+          }
+
+          return false;
+        });
+      }
+
+      return false;
+    }
+
+    //declaration of public functions && Object.keys(newPokemon).length===3 
+    function getAll(){
+        return pokemonList
+    };
+
+    function add(newPokemon){
+        //validation
+        if(typeof newPokemon==='object' && newPokemon.constructor === Object && areEqual(Object.keys(pokemonList[0]),Object.keys(newPokemon[0]))){
+          return pokemonList.push(newPokemon)  
+        }else{
+            console.log('Input is not an object or it\'s not equal to the object key properties of pokemonList.' )
+        }   
+    }
+
+    
+    //returning public functions
+    return{
+        getAll:getAll,
+        add:add
+    };
+
+    }
+    )();
+
+
+pokemonRepository.getAll().forEach(pokemon=>{
     //define pokemon name variable
     let name=pokemon.name;
     //define pokemon height variable
@@ -33,3 +76,4 @@ pokemonList.forEach(pokemon=>{
         document.write(name + '(height:' + height + ')<br>')
     };
     })
+
